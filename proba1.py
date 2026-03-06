ceeafaf2ea843d22f513ccc3687bb1bd4d9b3377
Hubert Markwart <martwy4@gmail.com>
2026-03-06 23:02:27 +0100

    second commit

--- proba1.py
+++ proba1.py
@@ -1,4 +1,9 @@
-a = (1,2,3,4,5)
-
-for a in a:
-    print('liczba')
+def order(cst_name, cst_order, *arg, **kwarg):
+    print(cst_name, 'ordered a', cst_order)
+    for arg in arg:
+        print(' - Add', arg)
+    for key, value in kwarg.items():
+        print(' - Add', key, ':', value)
+order('Tadzia', 'black coffee')
+order('Tadea', 'black coffe', milk='whole')
+order('Tadeusza','tea','none', sweetener='honey')
